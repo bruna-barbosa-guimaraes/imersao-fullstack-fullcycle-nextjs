15d3477cca26ea5e62195a9baae9700033aec1f0
brunaguimaraesssss <guimaraes.brunabarbosa@gmail.com>
2024-07-15 17:01:30 -0300

    first commit e card do evento

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,16 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    colors: {
+      primary: "rgba(24, 30, 36, 1)",
+      secondary: "#2a323c",
+    },
+    textColor: {
+      default: "#a6adbb",
+    },
+    gridTemplateColumns: {
+      "auto-fit-cards": "repeat(auto-fit, minmax(277px, 1fr",
+    }
   },
   plugins: [],
 };
